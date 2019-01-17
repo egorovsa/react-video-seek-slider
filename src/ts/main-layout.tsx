b42cbb28f6e7abfc69ea18b3249d29d96a19cb22
Egorov Sergey <Sergey.Egorov@noveogroup.com>
2019-01-18 00:15:04 +0200

    update styles use spaces instead of tabs

--- src/ts/main-layout.tsx
+++ src/ts/main-layout.tsx
@@ -1,51 +1,58 @@
 import * as React from 'react';
-import {VideoSeekSlider} from "./index";
+import { VideoSeekSlider } from "./index";
 
 export interface State {
-	currentTime: number,
-	test: boolean
+    currentTime: number,
+    progress: number,
+    test: boolean
 }
 
 export class AppComponent extends React.Component<any, State> {
-	state: State = {
-		currentTime: 110,
-		test: false
-	};
+    state: State = {
+        currentTime: 10,
+        progress: 0,
+        test: false
+    };
 
-	componentDidMount() {
-		// setTimeout(() => {
-		// 	this.setState({
-		// 		test: true
-		// 	});
-		// }, 4000);
-	}
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                currentTime: this.state.currentTime < 11150 ? this.state.currentTime + 10 : 0,
+            })
+        }, 100);
+        setInterval(() => {
+            this.setState({
+                progress: this.state.progress < 11150 ? this.state.progress + 3000 : 0,
+            })
+        }, 1000);
+    }
 
-	private getSlider() {
-		if (!this.state.test) {
-			return <VideoSeekSlider
-				max={11150}
-				currentTime={this.state.currentTime}
-				progress={4000}
-				onChange={(time: number, offsetTime: number) => {
-					this.setState({
-						currentTime: time // or offsetTime
-					} as State);
-				}}
-				offset={0}
-				limitTimeTooltipBySides={true}
-				secondsPrefix="00:00:"
-				minutesPrefix="00:"
-			/>;
-		}
-	}
+    private getSlider() {
+        if (!this.state.test) {
+            return <VideoSeekSlider
+                max={11150}
+                currentTime={this.state.currentTime}
+                progress={this.state.progress}
+                onChange={(time: number, offsetTime: number) => {
+                    this.setState({
+                        currentTime: time // or offsetTime
+                    } as State);
+                }}
+                offset={0}
+                limitTimeTooltipBySides={true}
+                secondsPrefix="00:00:"
+                minutesPrefix="00:"
+            />;
+        }
+    }
 
-	public render() {
-		return (
-			<div className="container">
-				<h1>React Video slider</h1>
+    public render() {
+        return (
+            <div className="container">
+                <h1>React Video slider</h1>
 
-				{this.getSlider()}
-			</div>
-		);
-	}
+                {this.getSlider()}
+            </div>
+        );
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,58 +2,58 @@ const path = require('path');
 const copyPluging = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: {
-		app: './src/ts/app.ts'
-	},
-	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "dist"),
-		publicPath: "/"
-	},
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		compress: true,
-		port: 3000
-	},
-	resolve: {
-		extensions: [
-			'.js',
-			'.ts',
-			'.tsx',
-			'.jsx',
-			'.styl'
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				loader: 'awesome-typescript-loader'
-			},
-			{
-				test: /\.styl?$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					{
-						loader: 'stylus-loader',
-						options: {}
-					}
-				]
-			},
-			{
-				test: /\.(jpe?g|png)$/,
-				loader: "file-loader"
-			}
-		]
-	},
-	plugins: [
-		new copyPluging([
-			{
-				from: "./src/index.html"
-			}
-		])
-	]
+    mode: "development",
+    entry: {
+        app: './src/ts/app.ts'
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3000
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.ts',
+            '.tsx',
+            '.jsx',
+            '.styl'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.styl?$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'stylus-loader',
+                        options: {}
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png)$/,
+                loader: "file-loader"
+            }
+        ]
+    },
+    plugins: [
+        new copyPluging([
+            {
+                from: "./src/index.html"
+            }
+        ])
+    ]
 };
