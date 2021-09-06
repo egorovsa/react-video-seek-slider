0109e395c3db5b0f80732ffed4762fc90637c12f
Sergey Egorov <egorovsa2@gmail.com>
2021-09-06 14:55:31 +0300

    Fix proudction build

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,8 +34,18 @@ module.exports = merge(common, {
   },
   externals: {
     // Don't bundle react or react-dom
-    react: "React",
-    "react-dom": "ReactDOM",
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
   },
   plugins: [
     new MiniCssExtractPlugin({
