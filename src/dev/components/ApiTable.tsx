17c87f62015d30539a71d8f22597f26ed21442dd
Sergey Egorov <sergey.egorov@noveogroup.com>
2025-07-31 18:57:07 +0300

    Update docs

--- src/dev/components/ApiTable.tsx
+++ src/dev/components/ApiTable.tsx
@@ -6,79 +6,124 @@ export const ApiTable: React.FC = () => {
           <tr>
             <th>Property</th>
             <th>Type</th>
-            <th>Required</th>
+            <th>Default</th>
             <th>Description</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><code>max</code></td>
+            <td>
+              <code>max</code>
+              <div className="required-badge">[required]</div>
+            </td>
             <td><code>number</code></td>
-            <td>✅</td>
-            <td>Maximum time in milliseconds</td>
+            <td>-</td>
+            <td>Video duration (in milliseconds)</td>
           </tr>
           <tr>
-            <td><code>currentTime</code></td>
+            <td>
+              <code>currentTime</code>
+              <div className="required-badge">[required]</div>
+            </td>
             <td><code>number</code></td>
-            <td>✅</td>
-            <td>Current time in milliseconds</td>
+            <td>-</td>
+            <td>Current video progress (in milliseconds)</td>
           </tr>
           <tr>
-            <td><code>onChange</code></td>
+            <td>
+              <code>onChange</code>
+              <div className="required-badge">[required]</div>
+            </td>
             <td><code>(time: number, offsetTime: number) =&gt; void</code></td>
-            <td>✅</td>
-            <td>Callback when time changes</td>
+            <td>-</td>
+            <td>Script to be run when thumb change position</td>
           </tr>
           <tr>
             <td><code>bufferTime</code></td>
             <td><code>number</code></td>
-            <td>❌</td>
-            <td>Buffer time in milliseconds</td>
-          </tr>
-          <tr>
-            <td><code>timeCodes</code></td>
-            <td><code>TimeCode[]</code></td>
-            <td>❌</td>
-            <td>Array of time codes</td>
-          </tr>
-          <tr>
-            <td><code>getPreviewScreenUrl</code></td>
-            <td><code>(hoverTime: number) =&gt; string</code></td>
-            <td>❌</td>
-            <td>Function to get preview URL</td>
+            <td>-</td>
+            <td>Current buffer progress (in milliseconds)</td>
           </tr>
           <tr>
             <td><code>hideThumbTooltip</code></td>
             <td><code>boolean</code></td>
-            <td>❌</td>
-            <td>Hide the tooltip on thumb hover</td>
-          </tr>
-          <tr>
-            <td><code>limitTimeTooltipBySides</code></td>
-            <td><code>boolean</code></td>
-            <td>❌</td>
-            <td>Limit tooltip position by track sides</td>
-          </tr>
-          <tr>
-            <td><code>secondsPrefix</code></td>
-            <td><code>string</code></td>
-            <td>❌</td>
-            <td>Prefix for seconds display</td>
-          </tr>
-          <tr>
-            <td><code>minutesPrefix</code></td>
-            <td><code>string</code></td>
-            <td>❌</td>
-            <td>Prefix for minutes display</td>
+            <td><code>false</code></td>
+            <td>Hide hover seek time</td>
           </tr>
           <tr>
             <td><code>offset</code></td>
             <td><code>number</code></td>
-            <td>❌</td>
-            <td>Time offset in milliseconds</td>
+            <td><code>0</code></td>
+            <td>When you need start slider with offset time</td>
+          </tr>
+          <tr>
+            <td><code>minutesPrefix</code></td>
+            <td><code>string</code></td>
+            <td><code>&apos;&apos;</code></td>
+            <td>When video duration is less than an hour you can use time prefix like &quot;0:&quot; so the time tooltip will show e.g &quot;0:25:23&quot;</td>
+          </tr>
+          <tr>
+            <td><code>secondsPrefix</code></td>
+            <td><code>string</code></td>
+            <td><code>&apos;&apos;</code></td>
+            <td>When video duration is less than one minute it&apos;s possible to use time prefix like &quot;0:00:&quot; and the time tooltip will show e.g &quot;0:00:10&quot;</td>
+          </tr>
+          <tr>
+            <td><code>limitTimeTooltipBySides</code></td>
+            <td><code>boolean</code></td>
+            <td><code>true</code></td>
+            <td>Limit the time tooltip position inside of the slider</td>
+          </tr>
+          <tr>
+            <td><code>timeCodes</code></td>
+            <td><code>TimeCode[]</code></td>
+            <td><code>undefined</code></td>
+            <td>Will divide slider into parts according to an array of times</td>
+          </tr>
+          <tr>
+            <td><code>getPreviewScreenUrl</code></td>
+            <td><code>(hoverTimeValue: number) =&gt; string</code></td>
+            <td><code>undefined</code></td>
+            <td>The callback function is going to be called each time when a slider is hovered. It will pass a current hover mouse time and expect a preview image url to be returned</td>
           </tr>
         </tbody>
       </table>
+      
+      <div className="api-section timecode-section">
+        <h3 className="timecode-title">
+          TimeCode Object
+        </h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Type</th>
+              <th>Default</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <code>fromMs</code>
+                <div className="required-badge">[required]</div>
+              </td>
+              <td><code>number</code></td>
+              <td>-</td>
+              <td>Time stamp in milliseconds from where the part is starting</td>
+            </tr>
+            <tr>
+              <td>
+                <code>description</code>
+                <div className="required-badge">[required]</div>
+              </td>
+              <td><code>string</code></td>
+              <td>-</td>
+              <td>Label that will appear on seeking tooltip</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }; 
